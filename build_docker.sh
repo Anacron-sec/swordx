c261ac141ccf3b4d6d50df1a5b6cf7a4421829b9
Michele Biondi (an4cr0n) <michelebiondi01@gmail.com>
2018-07-06 11:23:53 +0200

    Improved docker building script

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker container run --rm -v "$PWD":/usr/src/swordx -w /usr/src/swordx gcc:8.1.0 make
+docker container run --rm -v "$PWD":/usr/src/swordx -w /usr/src/swordx gcc:8.1.0 make clean && make
