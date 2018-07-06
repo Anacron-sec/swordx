df1116fe5118582e87d03a8dd82e17d58b266dc3
Michele Biondi (an4cr0n) <michelebiondi01@gmail.com>
2018-07-06 13:31:32 +0200

    Add argp

--- src/swordx.c
+++ src/swordx.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+#include <argp.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
+    return argp_parse (0, argc, argv, 0, 0, 0); 
     exit(EXIT_SUCCESS);
 }
