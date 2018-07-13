9beb5e2e3b24a62e474fefc42e871309bb2f0910
samuelecucchi <samuele.cucchi@studenti.unicam.it>
2018-07-13 11:42:11 +0200

    Fixed error on fileReader.c

--- src/fileReader.c
+++ src/fileReader.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-char *testFile(char *);
-char *readFile(char *);
+//#include "utils.h" //TODO integrare nel makefile
 
-char *testFile(char *path)
+char *openFile(char *);
+char *readFile(char *);
+int seekFile(FILE *);
+char *tok(char *);
+
+int main()
+{
+    printf("%s\n", openFile("//home//samuele//Desktop//text.txt"));
+    return 0;
+}
+
+char *openFile(char *path)
 {
     int fd = open(path, O_RDONLY);
     if (fd < 0)
@@ -19,6 +29,7 @@ char *readFile(char *path)
     FILE *ptrfile = fopen(path, "r");
     int size = seekFile(ptrfile);
     char *buf = (char *)malloc(size * sizeof(char));
+    //check_heap(buf);
     fread(buf, size * sizeof(char), 1, ptrfile);
     /*for (int i = 0; i < size; i++)
     {
@@ -33,4 +44,24 @@ int seekFile(FILE *ptrfile)
     fseek(ptrfile, 0L, SEEK_END);
     int size = ftell(ptrfile);
     fseek(ptrfile, 0L, SEEK_SET);
+    return size;
+}
+
+char *tok(char *str)
+{
+    const char delim[2] = " ";
+    char *token;
+
+    /* get the first token */
+    token = strtok(str, delim);
+
+    /* walk through other tokens */
+    while (token != NULL)
+    {
+        printf("%s\n", token);
+
+        token = strtok(NULL, delim);
+    }
+
+    return "done";
 }
