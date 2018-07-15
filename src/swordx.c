625c506dc5730309aa5dda132b8a93d7277f0f7c
Michele Biondi (an4cr0n) <michelebiondi01@gmail.com>
2018-07-15 13:56:37 +0200

    Increased version

--- src/swordx.c
+++ src/swordx.c
@@ -20,7 +20,7 @@ bool processing = false;
 char *output_file = "swordx.out";
 
 const char *argp_program_bug_address = "michelebiondi01@gmail.com";
-const char *argp_program_version = "SwordX version 0.3.0";
+const char *argp_program_version = "SwordX version 1.0.0";
 
 struct arguments {
     char *argz;
