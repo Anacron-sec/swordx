9515daa6be9d3a499211e20574e19dbb5958fb51
Michele Biondi (an4cr0n) <michelebiondi01@gmail.com>
2018-07-17 18:40:04 +0200

    Corrected version number

--- src/swordx.c
+++ src/swordx.c
@@ -25,7 +25,7 @@ bool processing = false;
 char *output_file = "swordx.out";
 
 const char *argp_program_bug_address = "michelebiondi01@gmail.com";
-const char *argp_program_version = "SwordX version 1.2.1";
+const char *argp_program_version = "SwordX version 1.2.0";
 
 struct arguments {
     char *argz;
